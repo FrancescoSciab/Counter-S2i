a2b12f8016f6b486482fef85908e750b72f88028
FrancescoSciab <francesco.sciabbarrasii@gmail.com>
2022-12-04 11:38:34 +0000

    readme

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Counter-S2i
 Project for Start2Impact University. Counter
+readme
+
+
+Ho realizzato un counter che permette di aumentare e diminuire il valore di span premendo rispettivamente il bottone con il simbolo più e quello con il simbolo meno.
+
+Non è necessario installare alcun software.
--- assets/JS/index.js
+++ assets/JS/index.js
@@ -1,12 +1,14 @@
+//Getting buttons 
 let plusBtn = document.getElementById("plus");
 let minusBtn = document.getElementById("minus");
 
-
+//Assigning to plus button a function which increase span
 plusBtn.onclick = function increaseValue() {
     let result = document.getElementById("result");
     return result.innerHTML++;
 }
 
+//Assigning to minus button a function which decrease span
 minusBtn.onclick = function decreaseValue() {
     let result = document.getElementById("result");
     return result.innerHTML--;
